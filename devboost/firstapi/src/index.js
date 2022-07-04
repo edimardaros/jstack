d6fcf911d0dfc265311761e33f9267a228cac0bc
Edimar Nascimento <edimar.daros@gmail.com>
2022-07-03 23:25:31 -0300

    JStak: query params, parsed deprec

--- devboost/firstapi/src/index.js
+++ devboost/firstapi/src/index.js
@@ -1,19 +1,25 @@
 const http = require('http');
-
+const url = require('url');
 const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
-  console.log(`Resquest method: ${request.method} | Endpoint: ${request.url}`);
+  // show parameters from request
+  // true modify query response to object. From query: `'order=desc'` to `query: [Object: null prototype] { order: 'desc' },`
+  const parsedUrl = url.parse(request.url, true);   
+  // console.log(parsedUrl);
+
+  console.log(`Resquest method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
 
   const route = routes.find((routeObj) => (
-    routeObj.endpoint === request.url && routeObj.method === request.method
+    routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
   ));
 
   if (route) {
+    request.query = parsedUrl.query;
     route.handler(request, response);
   } else {
       response.writeHead(404, {'Content-Type': 'text/html'});
-      response.end(`Cannot ${request.method}  ${request.url}`);
+      response.end(`Cannot ${request.method}  ${parsedUrl.pathname}`);
   }
 
   // if (request.url == '/users' && request.method == 'GET') {
