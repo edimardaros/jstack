742b9eec041e43968c8dedfa64a17af8973fb65f
Edimar Nascimento <edimar.daros@gmail.com>
2022-07-06 21:39:37 -0300

    JStack Node Express: O que são Middlewares? v1

--- devboost/mycontacts/src/index.js
+++ devboost/mycontacts/src/index.js
@@ -3,6 +3,14 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
-app.use(routes);
+
+app.use((request, response) => {
+  // Middleware 1
+  request.appId = 'MeuAppID';
+  response.send('Travado pelo middleware'); // Travar a requisição, não continuar para as rotas
+});
+
+app.use(routes); // Middleware 2
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
+// Middleware 1 -> Middleware 2 -> Middleware 3 -> Middleware 4
