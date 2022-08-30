f0121618e6d039c5f698cbb8f8ce4aa606c6a69a
Edimar Nascimento <edimar.daros@gmail.com>
2022-08-29 21:41:47 -0300

    Conectando o Node ao Postgres

---
+++ devboost/mycontacts/src/database/index.js
@@ -0,0 +1,20 @@
+const { Client } = require('pg');
+
+const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'docker',
+    database: 'mycontacts'
+});
+
+client.connect();
+
+async function Query(query) {
+    const { rows } = await client.query(query);
+    return rows;
+}
+
+Query('SELECT * FROM contacts;').then(console.log);
+// client.query('SELECT * FROM contacts;').then(console.log)
+// client.query('SELECT * FROM contacts;').then(result => console.log(result))
